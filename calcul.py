7d8fe02b7f445d36400a6c6982d1f5cfa92248b9
gaelmks <gaelmukassa41@gmail.com>
2026-03-26 13:28:12 +0100

    creation de la fenetre

--- calcul.py
+++ calcul.py
@@ -1,27 +1,20 @@
-import numpy
-def addition(a,b):
-    return a+b
-def soustraction(a,b):
-    return a-b
-def multiplication(a,b):
-    return a*b
-def division(a,b):
-    return a/b
-def reste(a,b):
-    return a%b
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel
 
-nombre1=float(input(" "))
-nombre2=float(input(" "))
-choix=" "
-if choix=='+':
-    reponse=addition(nombre1,nombre2)
-    print(reponse)
-elif choix=='-':
-    reponse=soustraction(nombre1,nombre2)
-    print(reponse)
-elif choix=='*':
-    reponse=multiplication(nombre1,nombre2)
-    print(reponse)
-elif choix=='/':
-    reponse= division(nombre1,nombre2)
-    print(reponse)
+class calculatrice(QWidget):
+    def __init__(self,win):
+        super().__init__()
+        self.win=win
+    
+    def build(self,color):
+        self.win.setWindowTitle("Calculatrice")
+        self.win.setGeometry(100,100,800,800)
+        self.win.setStyleSheet("background: " +color+ ";")
+
+if __name__=='__main__':
+    app=QApplication(sys.argv)
+    root=QWidget()
+    interface=calculatrice(root)
+    interface.build("gray")
+    root.show()
+    sys.exit(app.exec_())
