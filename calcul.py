9a2a8c02ccd54aa903b475cd5c63f4761ff4a1d9
gaelmks <gaelmukassa41@gmail.com>
2026-03-27 19:09:19 +0100

    ajout des touches 4 5 6

--- calcul.py
+++ calcul.py
@@ -23,14 +23,29 @@ class calculatrice(QWidget):
         self.btn1.setGeometry(0,300,100, 100)
 
         #bouton 2
-        self.btn1=QPushButton(self.win)
-        self.btn1.setText("2")
-        self.btn1.setGeometry(100,300,100, 100)
+        self.btn2=QPushButton(self.win)
+        self.btn2.setText("2")
+        self.btn2.setGeometry(100,300,100, 100)
 
         #bouton 3
-        self.btn1=QPushButton(self.win)
-        self.btn1.setText("3")
-        self.btn1.setGeometry(200,300,100, 100)
+        self.btn3=QPushButton(self.win)
+        self.btn3.setText("3")
+        self.btn3.setGeometry(200,300,100, 100)
+
+        #bouton 4
+        self.btn4=QPushButton(self.win)
+        self.btn4.setText("4")
+        self.btn4.setGeometry(0,400, 100, 100)
+
+        #bouton 5
+        self.btn5=QPushButton(self.win)
+        self.btn5.setText("5")
+        self.btn5.setGeometry(100, 400, 100, 100)
+
+        #bouton 6
+        self.btn6=QPushButton(self.win)
+        self.btn6.setText("6")
+        self.btn6.setGeometry(200, 400, 100, 100)
 
 if __name__=='__main__':
     app=QApplication(sys.argv)
